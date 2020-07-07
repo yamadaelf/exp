729425f889c988fb8af5aa53fd157c57d6238a3b
yamadaelf <yamadaelf>
2020-07-08 02:41:48 +0900

    BeanUtils简化封装

--- src/io/yamadaelf/web/servlet/LoginServlet.java
+++ src/io/yamadaelf/web/servlet/LoginServlet.java
@@ -3,12 +3,16 @@ package io.yamadaelf.web.servlet;
 import io.yamadaelf.dao.UserDao;
 import io.yamadaelf.domain.User;
 
+import org.apache.commons.beanutils.BeanUtils;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
 
 
 @WebServlet ("/loginServlet")
@@ -16,11 +20,15 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
        req.setCharacterEncoding("utf-8");
-       String username=req.getParameter("username");
-       String password=req.getParameter("password");
+        Map<String, String[]> map = req.getParameterMap();
        User loginUser=new User();
-        loginUser.setUsername(username);
-        loginUser.setPassword(password);
+        try {
+            BeanUtils.populate(loginUser,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
         UserDao dao=new UserDao();
         User user=dao.login(loginUser);
         if(user==null){
