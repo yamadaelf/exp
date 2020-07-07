13bab57fe9e161e767b7e76e99c68fd57db73c53
yamadaelf <yamadaelf>
2020-07-08 01:48:27 +0900

    连接数据库/servlet初步配置

---
+++ src/io/yamadaelf/dao/UserDao.java
@@ -0,0 +1,24 @@
+package io.yamadaelf.dao;
+
+import  io.yamadaelf.domain.User;
+import io.yamadaelf.util.JDBCUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;;
+
+public class UserDao {
+private JdbcTemplate template=new JdbcTemplate(JDBCUtils.getDataSource());
+
+    public User login(User loginUser){
+        try {
+            String sql ="select * from user where username = ? and password =?";
+            User user = template.queryForObject(sql,
+                    new BeanPropertyRowMapper<User>(User.class),
+                    loginUser.getUsername(), loginUser.getPassword());
+
+            return user;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/io/yamadaelf/web/servlet/LoginServlet.java
@@ -0,0 +1,41 @@
+package io.yamadaelf.web.servlet;
+
+import io.yamadaelf.dao.UserDao;
+import io.yamadaelf.domain.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet ("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       req.setCharacterEncoding("utf-8");
+       String username=req.getParameter("username");
+       String password=req.getParameter("password");
+       User loginUser=new User();
+        loginUser.setUsername(username);
+        loginUser.setPassword(password);
+        UserDao dao=new UserDao();
+        User user=dao.login(loginUser);
+        if(user==null){
+            req.getRequestDispatcher("/failServlet").forward(req,resp);
+        }
+        else {
+            req.setAttribute("user",user);
+            req.getRequestDispatcher("/successServlet").forward(req,resp);
+        }
+    }
+
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       this.doGet(req,resp);
+    }
+}
