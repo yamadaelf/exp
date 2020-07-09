cfd9b58763fd265fe8d328490a25df8f7d2ea975
yamadaelf <yamadaelf>
2020-07-10 00:47:44 +0900

    抽卡模拟（初版）

---
+++ web/draw.js
@@ -0,0 +1,74 @@
+
+    function draw(){
+
+        var a1 = new Array('凯露','可可萝','佩可莉姆','莉玛','玉泉美咲','双叶碧','绫濑由加莉','天野铃莓','风宫依里','栗林胡桃','未奏希','士条怜','草野优衣','春咲日和莉');
+
+        var a2 = new Array('宵浜深月','大神美冬','宫坂珠希','远见空花','三角千歌','柏崎栞','野户真阳','上喜忍','仓石惠理子','茜美美','喜屋武香织','美波铃奈','虹村雪','出云宫子','风宫茜里');
+
+        var a3 = new Array('姬塔','莫妮卡·拜斯温特','星野静流','白银纯','安芸真琴','藤堂秋乃','妮侬・朱贝尔','樱井望','佐佐木咲恋','支仓伊绪','柏崎初音','衣之咲璃乃','姬宫真步','柊杏奈');
+
+        $(".pic1 span").html("");
+        $(".pic2 span").html("");
+
+        var one = 0;
+        var two = 0;
+        var three = 0;
+
+        for(var i=1;i<11;i++)
+        {
+            var x = 1000;
+            var y = 0;
+            var q = parseInt(Math.random() * (x - y + 1));
+            var a = 1;
+            var ap = a1;
+
+            if(i==10){
+                if(q<=25){
+                    a = 3;
+                    ap = a3;
+                    three++;
+                }else{
+                    a = 2;
+                    ap = a2;
+                    two++;
+                }
+            }else{
+                if(q<=180 && q>25){
+                    a = 2;
+                    ap = a2;
+                    two++;
+                }else if(q<=25){
+                    a = 3;
+                    ap = a3;
+                    three++;
+                }else{
+                    one++;
+                }
+            }
+
+            var l = ap.length;
+            var girl = parseInt(Math.random()*(l-1));
+            var sp = 'images/star'+a+'/'+(girl+1)+'.png';
+            $(".p"+i).children('img').attr('src',sp);
+            if(a==3){
+                var ht = '<img src="'+sp+'">';
+                $(".pp3").append(ht);
+            }
+            $(".p"+i).children('i').text(ap[girl]);
+            for(var j=1;j<=a;j++){
+                var ht = '<img src="images/st.png" style="width:30px;padding:0;">';
+                $(".p"+i).children('span').append(ht);
+            }
+        }
+
+        var ss = $(".all span");
+        var s1 = $(".one span");
+        var s2 = $(".two span");
+        var s3 = $(".three span");
+        $(".all span");
+        $(".one span");
+        $(".two span");
+        $(".three span");
+
+    }
+
---
+++ web/draw.jsp
@@ -0,0 +1,60 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html lang="en" class="no-js">
+<head>
+<title>素敵な仲間が増えますよ!</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<script type="text/javascript" src="https://common.cnblogs.com/scripts/jquery-2.2.0.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="draw.css">
+	<script type="text/javascript" src="draw.js"></script>
+</head>
+<body>
+<div class="item-box ">
+	<div class="gacha-box mb-3">
+			<div class="d-flex flex-wrap gacha-content justify-content-center">
+
+		<div class="pic1">
+			<div  class="p1"><img src="images/b1.png"/><br><span></span><i></i></div>
+			<div  class="p2"><img src="images/b1.png"/><br><span></span><i></i></div>
+			<div  class="p3"><img src="images/b1.png"/><br><span></span><i></i></div>
+			<div  class="p4"><img src="images/b1.png"/><br><span></span><i></i></div>
+			<div  class="p5"><img src="images/b1.png"/><br><span></span><i></i></div>
+		</div>
+		<div class="pic2">
+			<div  class="p6"><img src="images/b1.png"/><br><span></span><i></i></div>
+			<div  class="p7"><img src="images/b1.png"/><br><span></span><i></i></div>
+			<div  class="p8"><img src="images/b1.png"/><br><span></span><i></i></div>
+			<div  class="p9"><img src="images/b1.png"/><br><span></span><i></i></div>
+			<div  class="p10"><img src="images/b2.png"/><br><span></span><i></i></div>
+		</div>
+			</div>
+		</div>
+</div>
+		<div class="draw">
+			<div><button onclick="draw()">10连</button></div>
+		</div>
+
+	<div style="color:red">
+		数据统计：
+	</div>
+	<div class="all">
+		总抽取次数：<span>0</span>
+	</div>
+	<div class="three">
+		三星数量：<span>0</span>
+	</div>
+	<div class="two">
+		二星数量：<span>0</span>
+	</div>
+	<div class="one">
+		一星数量：<span>0</span>
+	</div>
+	<div>
+		已获得三星：
+	</div>
+	<div class="pp3">
+		<img src="images/b3.png">
+	</div>
+	
+
+</body>
+</html>
